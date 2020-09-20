bb5f250fce2fec8fa5e499832f6cfd83bf81bafc
Afonso Machado <afonso777@hotmail.com>
2020-09-20 16:00:42 -0300

    implementado botão para uasr ao longo da aplicação

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+interface Props {
+  outlined?: boolean;
+}
+
+export default styled.button<Props>`
+  /* caso outlined seja true, o botão é transparente, se não tem um background azul */
+  background: ${(props) => (props.outlined ? 'transparent' : 'var(--twitter)')};
+  color: ${(props) => (props.outlined ? 'var(--twitter)' : 'var(--white)')};
+  border: ${(props) => (props.outlined ? '1px solid var(--twitter)' : 'none')};
+
+  padding: 16px;
+  border-radius: 25px;
+
+  font-weight: bold;
+  font-size: 15px;
+
+  cursor: pointer;
+  outline: 0;
+
+  /* Alterando cor do hover para os diferentes botões */
+  &:hover {
+    background: ${(props) =>
+      props.outlined
+        ? 'var(--twitter-dark-hover)'
+        : 'var(--twitter-light-hover)'};
+  }
+`;
