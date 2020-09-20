7e67c628b66736603a8e0c80a3fd6a9ebc72532e
Afonso Machado <afonso777@hotmail.com>
2020-09-20 16:59:44 -0300

    estrautura básica do componente Tweet

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -5,7 +5,40 @@ import { Container } from './styles';
 const Tweet: React.FC = () => {
   return (
     <Container>
-      <h1>Tweet</h1>
+      <RetweetIcon />
+      <Retweeted>Você retweetou</Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong>Rocketseat</strong>
+            <span>@rocketseat</span>
+            <Dot />
+            <time>20 de set</time>
+          </Header>
+
+          <Description>Foguete não tem ré 🚀</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              18
+            </Status>
+            <Status>
+              <RetweetIcon />
+              38
+            </Status>
+            <Status>
+              <LikeIcon />
+              125
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
     </Container>
   );
 };
