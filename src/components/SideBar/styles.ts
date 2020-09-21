171a9dbc37e194f9fb69ae450700899ae7972642
Afonso Machado <afonso777@hotmail.com>
2020-09-21 15:29:43 -0300

    implementado input do SideBar

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -2,11 +2,65 @@ import styled from 'styled-components';
 
 import { Search } from '../../styles/Icons';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  /* só aparece em desktop */
+  display: none;
 
-export const SearchWrapper = styled.div``;
+  @media (min-width: 1000px) {
+    display: flex;
+    flex-direction: column;
 
-export const SearchInput = styled.div``;
+    width: min(399px, 100%);
+  }
+`;
+
+export const SearchWrapper = styled.div`
+  padding: 10px 24px;
+  width: min(399px, 100%);
+
+  /* barra presa no topo da tela */
+  position: fixed;
+  top: 0;
+  z-index: 2;
+
+  background: var(--primary);
+  max-height: 57px;
+`;
+
+export const SearchInput = styled.input`
+  width: 100%;
+  height: 39px;
+
+  font-size: 14px;
+  padding: 0 10px 0 52px;
+  border-radius: 19.5px;
+  background: var(--search);
+
+  &::placeholder {
+    color: var(--gray);
+  }
+
+  /* posicionando a lupa dentro da barra */
+  ~ svg {
+    position: relative;
+    top: -33px;
+    left: 15px;
+    z-index: 1;
+
+    transition: 180ms ease-in-out;
+  }
+
+  outline: 0;
+
+  /* inserindo cor na borda e na lupa dentro do input */
+  &:focus {
+    border: 1px solid var(--twitter);
+
+    ~ svg {
+      fill: var(--twitter);
+    }
+  }
+`;
 
 export const SearchIcon = styled(Search)`
   width: 27px;
