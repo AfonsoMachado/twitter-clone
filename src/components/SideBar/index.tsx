085dd7491d828193a5e345cb98d465921685f4eb
Afonso Machado <afonso777@hotmail.com>
2020-09-21 14:54:49 -0300

    estrutura inicial do componente SideBar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
 
 const SideBar: React.FC = () => {
   return (
     <Container>
-      <h1>SideBar</h1>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <Body>
+        <p>
+          {
+            'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus illo maxime est delectus, neque commodi quae reiciendis, autem praesentium atque perferendis consequatur fuga dolores recusandae odit dolorum saepe consequuntur a.'
+          }
+        </p>
+      </Body>
     </Container>
   );
 };
--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+import { Search } from '../../styles/Icons';
 
+export const Container = styled.div``;
+
+export const SearchWrapper = styled.div``;
+
+export const SearchInput = styled.div``;
+
+export const SearchIcon = styled(Search)`
+  width: 27px;
+  height: 27px;
+
+  fill: var(--gray);
 `;
+
+export const Body = styled.div``;
